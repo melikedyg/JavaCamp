d13feddaa39cdaef9a2cdc6a99b119335cc57e83
Melike Yıldırım <59420123+melikedyg@users.noreply.github.com>
2021-12-24 01:03:57 +0300

    Delete campHomework directory

--- campHomework/src/StudentManager.java
+++
@@ -1,9 +0,0 @@
-
-public class StudentManager extends UserManager{
-	@Override
-	public void add(User student) {
-		System.out.println( student.firstName +" "+  student.lastName +" enrolled in "+ student.course+" course ");
-		
-
-}
-}
