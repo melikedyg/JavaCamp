a704ee23f71f5277bc766abc6aebc8b29599c597
Melike Yıldırım <59420123+melikedyg@users.noreply.github.com>
2021-12-24 01:04:46 +0300

    Add files via upload

---
+++ customerSystem/src/Program.java
@@ -0,0 +1,19 @@
+import java.rmi.RemoteException;
+
+import Adapters.MernisServiceAdapter;
+import Concrete.BaseCustomerManager;
+import Concrete.NeroCustomerManager;
+import Concrete.StarbucksCustomerManager;
+import Entities.Customer;
+
+public class Program {
+
+	public static void main(String[] args) throws NumberFormatException, RemoteException {
+		BaseCustomerManager customerManager=new StarbucksCustomerManager(new MernisServiceAdapter() );
+		customerManager.save(new Customer(1,"Zeynep","Yýldýrým",1985,"12345678956"));
+		
+		
+
+	}
+
+}
